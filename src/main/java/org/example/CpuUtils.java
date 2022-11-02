079cac8bd37d1bbd0810d7e4d67b899722e15dfc
Yazan Kittaneh <yazan.kittane@progressoft.com>
2022-11-02 16:59:07 +0100

    code cleanup and refactoring

--- src/main/java/org/example/CpuUtils.java
+++ src/main/java/org/example/CpuUtils.java
@@ -1,14 +1,7 @@
 package org.example;
 
-import org.example.util.StreamGobbler;
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
 public class CpuUtils {
@@ -19,14 +12,6 @@ public class CpuUtils {
     private static String CONTAINER_NAME;
 
     private static final String TASK_NAME = "TaskSimulator";
-
-    public static List<String> CPU_LIMIT_PID_LIST = new ArrayList<>();
-
-    public static String TASK_SIM_PID;
-
-    private static double tempGridPowerSum;
-
-    private static long tempGridPowerCount;
 
     public static void main(String[] args) {
         logger.info("CpuUtils.main called");
@@ -92,111 +77,6 @@ public class CpuUtils {
         } catch (Exception ex) {
             throw new RuntimeException("Error while getting container name", ex);
         }
-    }
-
-    private static void runCommand(String command) {
-        logger.info("runCommand: " + command);
-
-        try {
-            ProcessBuilder builder = new ProcessBuilder();
-            builder.command("bash", "-c", command);
-            Process process = builder.start();
-            StreamGobbler inputStreamGobbler = new StreamGobbler(process.getInputStream(), logger::info);
-            Executors.newSingleThreadExecutor().submit(inputStreamGobbler);
-            StreamGobbler errorStreamGobbler = new StreamGobbler(process.getErrorStream(), logger::warning);
-            Executors.newSingleThreadExecutor().submit(errorStreamGobbler);
-            int exitCode = process.waitFor();
-            logger.info("exitCode: " + exitCode);
-
-            process.destroy();
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException("Error while running command [" + command + "]", e);
-        }
-    }
-
-    public static void updateCpuLimitProcessId() {
-        logger.info("getting cpu limit process id");
-        try {
-            ProcessBuilder builder = new ProcessBuilder();
-            builder.command("bash", "-c", "pidof cpulimit");
-            Process process = builder.start();
-
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            String commandResult = bufferedReader.readLine();
-            logger.info("line: " + commandResult);
-            process.waitFor();
-            logger.info("exit: " + process.exitValue());
-            process.destroy();
-
-            logger.info("pidof cpulimit: " + commandResult);
-
-            if (isStringEmpty(commandResult))
-                logger.warning("blank result after running command 'pidof cpulimit'");
-            else
-                CPU_LIMIT_PID_LIST = new ArrayList<>(Arrays.asList(commandResult.split(" ")));
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException("Error while running command [pidof cpulimit]", e);
-        }
-    }
-
-    public static void handleCpuLevel(double gridPower) {
-        tempGridPowerCount++;
-        tempGridPowerSum += gridPower;
-
-        logger.info("+++ gridPower = [" + gridPower + "] - gridPowerAvg = [" + tempGridPowerSum / tempGridPowerCount + "]");
-
-        if (isCpuLimitingNeeded(gridPower)) {
-            // todo (high): consider taking the path of limiting cpu levels by a percentage relative to the gridPower value. Less power = Less CPU?
-
-            logger.info("gridPower [" + gridPower + "] is less than half of the average, going to limit cpu");
-            if (isCpuLimitProcessRunning()) {
-                // there is already a cpuLimit process alive, no need to do anything. Unless... (see the todo above)
-//                                killCpuLimitProcess();
-                logger.info("there is already a running cpulimit process. Not doing anything");
-            } else {
-                startCpuLimitProcess();
-                updateCpuLimitProcessId();
-            }
-        } else if (isCpuReleasingNeeded(gridPower)) {
-            logger.info("gridPower [" + gridPower + "] is larger than the average, going to kill cpulimit process (if running)");
-            if (isCpuLimitProcessRunning()) {
-                // kill running cpulimit process, and clear CPU_LIMIT_PID_LIST
-                killCpuLimitProcess();
-            }
-        }
-    }
-
-    public static void startCpuLimitProcess() {
-        logger.info("startCpuLimitProcess called");
-//        String command = "cpulimit -p " + getTaskSimPid() + " -l 50 -b";
-//        runCommand(command);
-        logger.info("startCpuLimitProcess finished");
-    }
-
-    public static void killCpuLimitProcess() {
-        logger.info("called killCpuLimitProcess");
-        runCommand("kill -9 " + String.join(" ", CPU_LIMIT_PID_LIST));
-
-        // send SIGCONT signal to the task sim process in case cpulimit is killed while task sim is stopped (SIGSTOP) (as part of the cpulimit operation)
-//        runCommand("kill -s SIGCONT " + getTaskSimPid());
-
-        logger.info("clearing CPU_LIMIT_PID_LIST");
-        CPU_LIMIT_PID_LIST.clear();
-    }
-
-    private static boolean isCpuLimitProcessRunning() {
-        return CPU_LIMIT_PID_LIST.size() > 0;
-    }
-
-    private static boolean isCpuReleasingNeeded(double gridPower) {
-        // todo: rewrite this part? relating to power model?
-        return gridPower > (tempGridPowerSum / tempGridPowerCount) * 1.5;
-    }
-
-    private static boolean isCpuLimitingNeeded(double gridPower) {
-        // todo: rewrite this part? relating to power model?
-        return gridPower < (tempGridPowerSum / tempGridPowerCount) / 2;
     }
 
     public static boolean isStringEmpty(String string) {
